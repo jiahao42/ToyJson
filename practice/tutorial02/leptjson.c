a22fc51e8108f1703deaa833d27aa9e9f00995c6
James <caterpillarous@yahoo.com>
2016-11-09 19:04:54 +0800

    tutorial2 problem3/4 solved

--- practice/tutorial02/leptjson.c
+++ practice/tutorial02/leptjson.c
@@ -1,10 +1,13 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
+#include <math.h>
+#include <errno.h>
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
+	double n;
     const char* json;
 }lept_context;
 
@@ -26,14 +29,45 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
 	c->json += i;
 	v->type = type;
 	return LEPT_PARSE_OK;
-	
 }
 
 static int lept_parse_number(lept_context* c, lept_value* v) {
     char* end;
-    /* \TODO validate number */
+	/* in case the number is 'nan' 'NAN' 'inf' 'INF' */
+	if(*c->json == 'i' || *c->json == 'I' || *c->json == 'n' || *c->json == 'N'){
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	/* only '.' can be after the '0' */
+	if (*c->json == '0'){
+		if (c->json[1] != '\0'){
+			if (c->json[1] != '.'){
+				return LEPT_PARSE_ROOT_NOT_SINGULAR;
+			}
+		}
+	}
+	/* number never start with '.' or '+' */
+    if (*c->json == '.' || *c->json == '+'){
+		return LEPT_PARSE_INVALID_VALUE;
+	}
+	/* only digit can be after the '.' */
+	size_t i = 0;
+	while(c->json[i] != '\0'){
+		if(c->json[i] == '.'){
+			i++;
+			if((c->json[i] - '0') > 9 || (c->json[i] - '0') < 0){
+				return LEPT_PARSE_INVALID_VALUE;
+			}
+			break;
+		}
+		i++;
+	}
+	/* in case the number is too big */
+	errno = 0;
     v->n = strtod(c->json, &end);
-    if (c->json == end)
+	if (v->n == HUGE_VAL || errno == ERANGE){
+		return LEPT_PARSE_NUMBER_TOO_BIG;
+	}
+    if (c->json == end)/* if the str not change at all, it means there is no number */
         return LEPT_PARSE_INVALID_VALUE;
     c->json = end;
     v->type = LEPT_NUMBER;
@@ -45,8 +79,8 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 't':  return lept_parse_literal(c,v,"true",LEPT_TRUE);
 		case 'f':  return lept_parse_literal(c,v,"false",LEPT_FALSE);
 		case 'n':  return lept_parse_literal(c,v,"null",LEPT_NULL);
-        default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+		default:   return lept_parse_number(c, v);
     }
 }
 
